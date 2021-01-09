ec0b84bbf199e812cdd190bde605528f8c397244
Akaporn Katip <akaporn@riverpark.co.th>
2021-01-10 02:15:07 +0700

    fetch product

--- src/pages/Dashboard/Index.vue
+++ src/pages/Dashboard/Index.vue
@@ -2,7 +2,7 @@
   <q-page padding>
     <div class="column">
       <div class="col">
-        <data-table style="height: 520px"  title="Sell History">
+        <data-table style="height: 520px" title="Sell History">
           <template v-slot:no-data>
             No Sell Data
           </template>
@@ -10,7 +10,12 @@
       </div>
     </div>
     <div class="col q-pt-md">
-      <data-table style="height: 520px" title="All Products" :columns="products_columns" :data="products">
+      <data-table
+        style="height: 520px"
+        title="All Products"
+        :columns="products_columns"
+        :data="products"
+      >
         <template v-slot:no-data>
           No Product Data
         </template>
@@ -22,17 +27,25 @@
 <script>
 import products from "models/products";
 import DataTable from "src/components/data-table/DataTable.vue";
+import { Loading } from "quasar";
 export default {
   name: "index",
   components: { DataTable },
+  async preFetch({ store }) {
+    let snap_products = await store.$db.firebase.$firestore
+      .collection("products")
+      .get();
+    products.create({
+      data: snap_products.docs.map(doc => doc.data())
+    });
+  },
   data() {
     return {
-      products: products.query().all(),
       products_columns: products.getColumns()
     };
   },
-  preFetch() {
-    products.getSampleData()
+  computed: {
+    products: () => products.query().all()
   }
 };
 </script>
