67d3c5919373cdedd78ab3e98bd6403fdc89bd79
Akaporn Katip <akaporn@riverpark.co.th>
2021-01-09 15:51:41 +0700

    create application layout

---
+++ src/layouts/ApplicationLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-layout view="hhh lpr fFf">
+    <q-header class="bg-transparent">
+      <app-tool-bar>
+        <logout-btn slot="append" />
+      </app-tool-bar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer class="flex flex-center bg-transparent text-black"> </q-footer>
+  </q-layout>
+</template>
+
+<script>
+import AppToolBar from "components/AppToolBar";
+import LogoutBtn from 'src/components/LogoutBtn.vue';
+
+export default {
+  name: "MainLayout",
+  components: { AppToolBar, LogoutBtn },
+  data() {
+    return {};
+  }
+};
+</script>
